432cb148d4c6c3e45f89eca79d74cb3a04ccbdc9
hf <3025463144@qq.com>
2018-10-26 09:08:49 +0800

    Idea整合GitHub测试

--- src/main/java/test/HelloWorld.java
+++ src/main/java/test/HelloWorld.java
@@ -2,6 +2,6 @@ package test;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("new Hello Git!");
     }
 }
